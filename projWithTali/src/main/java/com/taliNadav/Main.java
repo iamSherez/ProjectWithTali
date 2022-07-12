d051e3dd20458e0f3f1beabb2ef3cb2fbaad2da9
iamSherez <iam_sherez@hotmail.com>
2022-07-12 21:38:15 +0300

    3rd Commit

--- projWithTali/src/main/java/com/taliNadav/Main.java
+++ projWithTali/src/main/java/com/taliNadav/Main.java
@@ -3,5 +3,7 @@ package com.taliNadav;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Nadav!!");
+        System.out.println("Sup Tali ?");
     }
+
 }
